f9eeb8ec120188d0643ceb1b91181cb005df01bd
softveda-x <merajalam8294@gmail.>
2026-02-21 18:43:35 +0530

    First commit

--- Variable_Operators_L1/Question_02.java
+++ Variable_Operators_L1/Question_02.java
@@ -1,1 +1,9 @@
 //Swap two numbers without using a third variable.?
+package Variable_Operators_L1;
+class Question_02
+{
+    public static void main(String[] args) {
+        
+        System.out.println("Meraj Alam");
+    }
+}
